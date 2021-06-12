45d41ebaac6856f2fafb94d9d154fb620e811f29
AlexMuñoz <munozmiguelalexis@gmail.com>
2021-06-12 20:07:56 -0300

    primer commit

--- index.js
+++ index.js
@@ -25,6 +25,28 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+function main() {
+  const lista = document.querySelector(".lista");
+  const itemsLista = document.querySelectorAll(".lista li");
+
+  // itemsLista.forEach((e) => {
+  //   e.remove();
+  // });
+
+  for (let e of itemsLista) {
+    e.remove();
+  }
+
+  for (let item of cosasQueAprendimos) {
+    const newLiEl = document.createElement("li");
+    newLiEl.textContent = item.tema;
+    const claseQueLeVaAAsignarAlLi = item.class || "item";
+
+    newLiEl.classList.add(claseQueLeVaAAsignarAlLi);
+    console.log(newLiEl);
+
+    lista.appendChild(newLiEl);
+  }
+}
 
 main();
